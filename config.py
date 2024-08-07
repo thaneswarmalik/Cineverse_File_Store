488bcea7adf69c1fa21b81a09bfbe659ee66a9f8
thaneswarmalik <157218198+thaneswarmalik@users.noreply.github.com>
2024-08-07 13:01:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://cineverse-file-store.onrender.com/")
+URL = environ.get("URL", "")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
