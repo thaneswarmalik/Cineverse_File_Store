49f755bfff3fd3c0473e2ca29a5820c534a0d49c
thaneswarmalik <157218198+thaneswarmalik@users.noreply.github.com>
2024-08-07 16:08:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6033278775').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "@Traning01Bot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "Traning01Bot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
